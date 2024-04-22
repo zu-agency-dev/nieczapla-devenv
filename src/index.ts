432aadfdccba9968d7a3db7f7690f6feaa8866d6
Dariusz Sułtanowski <darek@zu.agency>
2024-04-22 09:54:29 +0200

    pre-release: 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  console.log('hello, localhost!');
 
-  document.addEventListener('load', => {
-    const heroImageOverlay = document.querySelector('.hero-background_image-overlay');
-    heroImageOverlay.style.background = '"' + heroImageOverlay.getAttribute('data-color') + '";';
-  });
-});
